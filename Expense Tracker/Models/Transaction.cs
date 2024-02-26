b8dfcc19930e875b1544f7a5585b6f7b138e6d8b
Siddhi <siddhishirke079@gmail.com>
2024-02-26 15:31:32 +0530

    Add project files.

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key] 
+        public int TransactionId { get; set; }
+
+        public int Amount { get; set; }
+        public string? Note { get; set; }
+
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+    }
+}
